498eee0bf651e29151df8af9342cc42a3dece125
Robby Bourne <robbybourne1@gmail.com>
2017-08-17 10:37:47 -0400

    Non Adjacent threes function

--- Program.cs
+++ Program.cs
@@ -10,13 +10,16 @@ namespace array_dotnet
         {
             Console.WriteLine("Hello World!");
             var firstNumlist = new List<int>();
+            firstNumlist.Add(3);
             firstNumlist.Add(1);
-            firstNumlist.Add(2);
-            firstNumlist.Add(8);
-            firstNumlist.Add(7);
-            firstNumlist.Add(9);
+            firstNumlist.Add(3);
+            firstNumlist.Add(1);
+            firstNumlist.Add(3);
             firstNumlist.Add(13);
-            firstNumlist.Add(16);
+            firstNumlist.Add(3);
+            var  myArray = firstNumlist.ToArray();
+            Console.WriteLine(countMults(myArray, 2));
+            Console.WriteLine(hasThreNonAdjacentThrees(myArray));
         }
 
         public static int countMults(int[] nums, int divisibility)
@@ -33,9 +36,31 @@ namespace array_dotnet
             return divisibleCount;
         }
 
-        publlc static bool hasThreNoneAdjacentThrees(int[] nums) 
+        public static bool hasThreNonAdjacentThrees(int[] nums) 
         {
-            return true; 
+            var threeCount = 0;
+            foreach (var item in nums)
+            {
+                if (item == 3)
+                {
+                    threeCount++;
+                }
+            }
+            if (threeCount != 3)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < nums.Length-1; i++)
+                {
+                    if (nums[i] == 3 && nums[i+1] == 3)
+                    {
+                        return false; 
+                    }
+                }
+                return true;
+            }
         } 
 
         public static bool containsString(char[] letters, string word)
